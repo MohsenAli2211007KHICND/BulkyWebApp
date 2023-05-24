195864f23f6c56117304e3a8930e6a227b9111e3
Mohsen Ali <mohsen.ali@xloopdigital.com>
2023-05-24 12:19:37 +0500

    Removed Bulky Razor project

--- Bulky.DataAccess/DbInitializer/DbInitializer.cs
+++ Bulky.DataAccess/DbInitializer/DbInitializer.cs
@@ -29,44 +29,44 @@ namespace Bulky.DataAccess.DbInitializer
         public void Initialize()
         {
 
-            //    //migration if they are not applied
-            //    try
-            //    {
-            //        if (_db.Database.GetPendingMigrations().Count() > 0)
-            //        {
-            //            _db.Database.Migrate();
-            //        }
-            //    }
-            //    catch (Exception ex) { }
+            //migration if they are not applied
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex) { }
 
 
-            //    //create roles if they are not created
-            //    if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
-            //    {
-            //        _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
-            //        _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
-            //        _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
-            //        _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+            //create roles if they are not created
+            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
 
 
-            //        //if roles are not created, then we will create admin user as well
-            //        _userManager.CreateAsync(new ApplicationUser
-            //        {
-            //            UserName = "adminbaloch@gmail.com",
-            //            Email = "adminbaloch@gmail.com",
-            //            Name = "Mohsen Ali",
-            //            PhoneNumber = "03162477395",
-            //            Region = "Balochistan,Pakistan",
-            //            Country = "Pakistan",
-            //            PostalCode = "25849",
-            //            City = "Turbat"
-            //        }, "Gulbuledai@1").GetAwaiter().GetResult();
+                //if roles are not created, then we will create admin user as well
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "adminbaloch@gmail.com",
+                    Email = "adminbaloch@gmail.com",
+                    Name = "Mohsen Ali",
+                    PhoneNumber = "03162477395",
+                    Region = "Balochistan,Pakistan",
+                    Country = "Pakistan",
+                    PostalCode = "25849",
+                    City = "Turbat"
+                }, "Gulbuledai@1").GetAwaiter().GetResult();
 
-            //        ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "adminbaloch@gmail.com");
-            //        _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
-            //    }
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "adminbaloch@gmail.com");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+            }
 
-            ////return;
+            //return;
 
         }
     }
